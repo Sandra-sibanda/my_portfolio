5f6974c3033744c9bc21ee41a035764c25478aea
Sandra-sibanda <sandra.sibanda992@gmail.com>
2024-09-13 22:00:34 +0200

    my portfolio

---
+++ process_form.php
@@ -0,0 +1,9 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    // Code to process form data, like sending an email or storing in a database
+}
+?>
